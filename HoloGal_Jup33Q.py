94f28fc8300d1eac7545e38b16abccb2c348df37
Jup33Q <zxgzg@163.com>
2025-01-23 15:33:30 +0800

    Add files via upload

---
+++ HoloGal_Jup33Q.py
@@ -0,0 +1,72 @@
+import streamlit.components.v1 as components
+import streamlit as st
+
+with st.sidebar:
+    st.markdown('## Jup33Q')
+    frameLogo=components.html(
+        '''
+<div class="lkg-blocks-player" style="padding:133.333% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/5148fd7b-a2a4-4650-b215-1bc9b2ce53e1" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+        ''',
+        height=400,width=300
+    )
+    st.divider()
+    st.write('Author: Jup33Q, for more information visit https://github.com/Jup33Q?tab=repositories')
+
+st.title('My Holo Gallery')
+
+
+st.markdown('''
+## Engine
+            ''')
+st.divider()
+st.write('created in Jan/23/2025')
+frame1_0=components.html(
+    '''
+<div class="lkg-blocks-player" style="padding:133.333% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/512f1a6a-0efa-41c9-99b7-2ca6584761c3" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+    ''',
+    height=1000,width=800
+)
+
+st.markdown('''
+## Gear_and_Balls
+            ''')
+st.divider()
+st.write('created in Jan/22/2025')
+frame1_1=components.html(
+    '''
+<div class="lkg-blocks-player" style="padding:133.333% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/d6600b84-f339-4a17-a7cc-8b7750277671" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+    ''',
+    height=1000,width=800
+)
+
+frame1_2=components.html(
+    '''
+<div class="lkg-blocks-player" style="padding:133.333% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/202419c3-bdd0-4b00-8f17-824a7b043325" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+    ''',
+    height=1000,width=800
+)
+
+st.markdown('''
+## Head
+created in year 2024
+''')
+st.divider()
+st.write('created in year 2024')
+
+frame2=components.html(
+    '''
+<div class="lkg-blocks-player" style="padding:133.333% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/219a659d-aa21-4c95-88d6-a5a74aff314f" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>   
+
+    ''',
+    height=1000,width=800
+)
+
+frame3=components.html(
+    '''
+<div class="lkg-blocks-player" style="padding:133.333% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/476d207a-9662-402e-9399-f303e7a43c21" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+    ''',
+    height=1000,width=800
+)
+
+
+
