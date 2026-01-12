dd36d3611192fc32a12e70306e7faeeb3738ccd5
Jup33Q <zxgzg@163.com>
2026-01-12 17:27:21 +0800

    The New Holo Model Collections
    More Holo-Graphic Model Practice

---
+++ HoloGal_V2_Jup33Q.py
@@ -0,0 +1,89 @@
+import streamlit.components.v1 as components
+import streamlit as st
+
+
+# Title & Sidebar
+
+with st.sidebar:
+    st.markdown('## Jup33Q')
+    frameLogo=components.html(
+        '''
+<div class="lkg-blocks-player" style="padding:133.333% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/5148fd7b-a2a4-4650-b215-1bc9b2ce53e1" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+        ''',
+        height=400,width=300
+    )
+    st.divider()
+    st.write('Author: Jup33Q, for more information visit https://github.com/Jup33Q?tab=repositories')
+
+    st.write('This is a Collection of My modeling project crafted with 3DMax,textured with BodyPaint, shaded and quilt-rendered in Unity 2022 with help of shader graph and plugins from LKG.')
+    st.write('Quilts are stored on LKG Block: https://blocks.glass/library')
+
+st.title('My Holo Gallery')
+# End Of Title & Sidebar
+
+st.divider()
+
+# Body
+
+# Simple House Garden
+st.markdown('''
+## Simple House Garden (The Start of Very Begin)
+''')
+
+st.write('created in late Dec 2025 to Early Jan 2026')
+
+components.html(
+'''
+<div class="lkg-blocks-player" style="padding:56.18% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/e38b6239-ef15-44a7-8369-8f71eda9d3ab" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+''',
+    height=480, width=720
+)
+# End of Simple House Garden
+
+st.divider()
+# Weapon1
+st.markdown('''
+## Weapon_1s
+''')
+st.write('created in Mid January 2026')
+
+components.html(
+'''
+<div class="lkg-blocks-player" style="padding:178.571% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/7b3cc8d7-d45a-4de1-929e-7c21c689036d" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+''',
+    height=960, width=540
+)
+
+components.html(
+    '''
+<div class="lkg-blocks-player" style="padding:178.571% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/168e33f8-fc4d-4627-8155-fd4b5c24ce87" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+    ''',
+    height=960, width=540
+)
+# EndOfWeapon1
+
+st.divider()
+
+# Tree1
+st.markdown('''
+## Tree_1
+            ''')
+
+st.write('created in Mid January 2026')
+
+components.html(
+    '''
+<div class="lkg-blocks-player" style="padding:178.571% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/9f112eac-7e73-414c-9e33-557397791e0c" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>
+    ''',
+    height=960, width=540
+)
+
+components.html(
+    '''
+<div class="lkg-blocks-player" style="padding:178.571% 0 0 0;position:relative;"><iframe src="https://blocks.glass/embed/292718ae-3724-4af5-b415-958f7397c3a3" frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;" allow="autoplay; encrypted-media; xr-spatial-tracking; accelerometer; gyroscope; magnetometer" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true"></iframe></div>    ''',
+    height=960, width=540
+)
+# EndOfTree1
+
+
+#EndOfBody
